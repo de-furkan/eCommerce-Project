e535a95dfba026e9eae91b42352666f3721dfbbb
Furkan O <85889728+de-furkan@users.noreply.github.com>
2024-10-08 21:20:21 +0100

    Updated utilities package and added:
        - BrowserType enum class which manages the different browsers for
          testing
        - DriverUtil class responsible for setting up a Thread-Safe WebDriver
          instance.
    
        The BrowserType enum class provides a list of different browsers from which
        the DriverUtil class references. This provides a way to keep the
        files modular and readable.
    
        The DriverUtil class uses the BrowserType enum class to reference the
        different browsers to then setup a thread-safe execution environment
        when the tests are done in parallel mode. The DriverUtil class also
        ensures that each WebDriver instance maximises the window when launched.
        The class also provides a method for quitting the driver instance,
        removing it from the ThreadLocal storage and freeing up the resources.

--- src/test/java/utilities/BrowserType.java
+++ src/test/java/utilities/BrowserType.java
@@ -1,2 +1,52 @@
-package utilities;public enum BrowserType {
+package utilities;
+
+import java.util.Objects;
+
+public enum BrowserType {
+    CHROME,
+    FIREFOX,
+    EDGE,
+    SAFARI,
+    EXPLORER,
+    CHROME_HEADLESS,
+    FIREFOX_HEADLESS,
+    EDGE_HEADLESS;
+
+    public static String getClassPath() {
+        String path = Objects.requireNonNull(BrowserType.class.getCanonicalName());
+        return "Path: " + path;
+    }
+
+    /*
+     ***************************************************************************************
+     *                  ⚠️ WARNING INFORMATION FOR                                         *
+     *               PARALLEL EXECUTION & HEADLESS MODES                                   *
+     !**************************************************************************************
+     !      >> PARALLEL EXECUTION SUPPORT: <<                                              *
+     *                                                                                     *
+     !      Parallel Execution does not work on SAFARI. Attempting parallel testing        *
+     !      with multiple WebDriver instances of SAFARI may throw an error. Please note    *
+     !      as of now SAFARI only supports a SINGLE WebDriver instance at a time.          *
+     !      While INTERNET EXPLORER does support parallel execution, it is still a legacy  *
+     !      browser and does have its challenges when parallel testing or using grid.      *
+     *                                                                                     *
+     !              -> SAFARI                       ❌                                     *
+     !              -> INTERNET EXPLORER            ⚠️                                     *
+     *                                                                                     *
+     ?      To Read More:                                                                  *
+     ?      https://developer.apple.com/documentation/webkit/about_webdriver_for_safari    *
+     !                                                                                     *
+     !      >> HEADLESS MODE NATIVE SELENIUM SUPPORT: <<                                   *
+     *                                                                                     *
+     !      Currently, --headless mode is NOT natively supported for the following         *
+     !      browsers by Selenium:                                                          *
+     !                                                                                     *
+     !              -> SAFARI                       ❌                                     *
+     !              -> INTERNET EXPLORER            ❌                                     *
+     *                                                                                     *
+     ?      To Read More:                                                                  *
+     ?          [1] https://github.com/SeleniumHQ/selenium/issues/5985                     *
+     ?          [2] https://stackoverflow.com/a/46649328                                   *
+     !**************************************************************************************
+     */
 }
--- src/test/java/utilities/DriverUtil.java
+++ src/test/java/utilities/DriverUtil.java
@@ -1,2 +1,208 @@
-package utilities;public class DriverUtil {
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ThreadGuard;
+
+public class DriverUtil {
+
+    /*
+     *****************************************
+     *          1. CONSTANTS
+     *****************************************
+     */
+    // Default symbols for success, warning or error messages.
+    private static final String SUCCESS_MESSAGE_SYMBOL = "🟢";
+    private static final String WARNING_MESSAGE_SYMBOL = "🟡";
+    private static final String ERROR_MESSAGE_SYMBOL   = "🔴";
+
+    // Thread-safety for driver instance, useful for parallel testing modes.
+    private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
+
+    /*
+     *****************************************
+     *          2. Static fields
+     *****************************************
+     */
+    // Store browser type in variable.
+    private static String browserName;
+
+    /*
+     *****************************************
+     *          3. Instance fields
+     *****************************************
+     */
+
+    /*
+     *****************************************
+     *          4. Constructors
+     *****************************************
+     */
+    // No instantiation of this class, Singleton.
+    private DriverUtil() {};
+
+    /*
+     *****************************************
+     *          5. Static block
+     *****************************************
+     */
+
+    /*
+     *****************************************
+     *          6. Public methods
+     *****************************************
+     */
+    /**
+     * <h2>{@code setDriverInstance(...)}: Sets the WebDriver instance for the specified browser type.</h2>
+     * <p>
+     * This method initialises the WebDriver if it is not already set,
+     * ensuring that only one instance is created per thread using
+     * ThreadLocal. It sets the value for the {@code driver} field and returns the WebDriver instance.
+     * </p>
+     *
+     * <p>
+     * This method requires a BrowserType argument, provided by the BrowserType enum class.
+     * </p>
+     *
+     * @param browser the type of browser for which the WebDriver is to be initialised (e.g., CHROME, FIREFOX).
+     * @return the initialised WebDriver instance for the specified browser.
+     * @see BrowserType
+     * @see #initDriver(BrowserType)
+     */
+    public static WebDriver setDriverInstance(BrowserType browser) {
+        browserName = browser.toString();
+        if (DRIVER.get() == null) {
+            initDriver(browser);
+        }
+        return DRIVER.get();
+    }
+    /**
+     * <h2>{@code quitDriver()}: Quits the WebDriver instance and removes it from the ThreadLocal storage.</h2>
+     * <p>
+     * This method checks if the current WebDriver instance is active. If so, it quits the
+     * WebDriver and removes it from the ThreadLocal storage to clean up resources. A success
+     * message is printed upon successful termination.
+     * </p>
+     */
+    public static void quitDriver() {
+        try {
+            if (DRIVER.get() != null) {
+                DRIVER.get().quit();
+                DRIVER.remove();
+                System.out.println(SUCCESS_MESSAGE_SYMBOL + browserName + " driver successfully quit.");
+                return;
+            }
+            throw new NoSuchMethodException();
+        } catch (NoSuchMethodException e) {
+            System.out.println(ERROR_MESSAGE_SYMBOL + " " + "DRIVER is null. Please set a driver instance.");
+        }
+    }
+    /*
+     *****************************************
+     *          7. Protected methods
+     *****************************************
+     */
+
+    /*
+     *****************************************
+     *          8. Private methods
+     *****************************************
+     */
+    /**
+     * <h2>{@code initDriver(...)}: Initialise the WebDriver for the specified browser type.</h2>
+     * <p>
+     * This method sets up the WebDriver based on the browser type provided. It supports
+     * both standard and headless modes for various browsers, including Chrome, Firefox, Edge,
+     * Safari, and Internet Explorer, all provided by the {@code BrowserType} enum class.
+     * The WebDriver instance is protected by {@code ThreadGuard} to ensure thread safety in parallel
+     * execution environments.
+     * </p>
+     *
+     * <p>
+     * If the browser type is invalid or unrecognised, a {@code RuntimeException} is thrown
+     * with a descriptive message. The method also maximises the browser window after
+     * initialisation.
+     * </p>
+     *
+     * <p>
+     * This method requires a BrowserType argument, provided through the BrowserType enum class.
+     * </p>
+     *
+     * @param browser the browser type for which the WebDriver is initialised (e.g., CHROME, FIREFOX, EDGE, etc.).
+     * @throws RuntimeException if an unsupported or unrecognised browser type is provided.
+     * @see BrowserType
+     */
+    private static void initDriver(BrowserType browser) {
+        String successMessage = SUCCESS_MESSAGE_SYMBOL + browser + " " + "driver successfully initialised.";
+        String headless = "--headless";
+        String defaultMessage =
+                WARNING_MESSAGE_SYMBOL
+                        + " "
+                        + "Driver definition unclear: "
+                        + "'" + browser + "'."
+                        + " " + "Please ensure a valid browser is selected."
+                        + " " + "Check the following class for details: " + BrowserType.getClassPath();
+        switch (browser) {
+            case CHROME:
+                DRIVER.set(ThreadGuard.protect(new ChromeDriver()));
+                break;
+            case FIREFOX:
+                DRIVER.set(ThreadGuard.protect(new FirefoxDriver()));
+                break;
+            case EDGE:
+                DRIVER.set(ThreadGuard.protect(new EdgeDriver()));
+                break;
+            case SAFARI:
+                DRIVER.set(ThreadGuard.protect(new SafariDriver()));
+                break;
+            case EXPLORER:
+                DRIVER.set(ThreadGuard.protect(new InternetExplorerDriver()));
+                break;
+            case CHROME_HEADLESS:
+                DRIVER.set(ThreadGuard.protect(
+                        new ChromeDriver(
+                                new ChromeOptions().addArguments(
+                                        headless
+                                )
+                        )
+                ));
+                break;
+            case FIREFOX_HEADLESS:
+                DRIVER.set(ThreadGuard.protect(
+                        new FirefoxDriver(
+                                new FirefoxOptions().addArguments(
+                                        headless
+                                )
+                        )
+                ));
+                break;
+            case EDGE_HEADLESS:
+                DRIVER.set(ThreadGuard.protect(
+                        new EdgeDriver(
+                                new EdgeOptions().addArguments(
+                                        headless
+                                )
+                        )
+                ));
+                break;
+            default:
+                throw new RuntimeException(defaultMessage);
+        }
+        System.out.println(successMessage);
+
+        // maximise the browser window
+        DRIVER.get().manage().window().maximize();
+    }
+    /*
+     *****************************************
+     *          9. Override methods
+     *****************************************
+     */
 }
