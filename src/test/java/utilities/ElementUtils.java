31ad63f6c59175dcf1b90fd2dd6a8b988ace0b5c
Furkan O <85889728+de-furkan@users.noreply.github.com>
2024-10-25 17:49:24 +0100

    Add`selectRandomDropdownOption()`
    - Randomly select an enabled dropdown option.
    - Logs success when an enabled option is selected and warns if disabled options are encountered.
    - Handles exceptions using `WaitUtil` for non-clickable dropdowns or null elements.

--- src/test/java/utilities/ElementUtils.java
+++ src/test/java/utilities/ElementUtils.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class ElementUtils {
     /*
@@ -222,6 +224,45 @@ public class ElementUtils {
         if (!dropdown.getOptions().isEmpty()) {
             dropdown.selectByValue(value);
             LOGGER.success("Successfully selected dropdown option by value:" + " " + value);
+        }
+    }
+    /**
+     * <h2>{@code selectRandomDropdownOption(...)}: Selects a random enabled option from a dropdown menu within a set number of attempts.</h2>
+     *
+     * <p>This method waits for the specified dropdown element to become clickable within the provided timeout.
+     * It then attempts to select a random enabled option from the dropdown, with a maximum of 5 tries.
+     * If an enabled option is successfully selected, the option's text is logged. If a disabled option is encountered,
+     * the method logs a warning and retries up to the maximum attempts.</p>
+     *
+     * <p>This method assumes that exceptions are handled through the {@link WaitUtil#waitForClickableElement(WebElement, int)}</p>
+     *
+     * @param element the {@link WebElement} representing the dropdown menu
+     * @param timeout the maximum wait time, in seconds, for the dropdown to become clickable
+     *
+     */
+    public static void selectRandomDropdownOption(WebElement element, int timeout) {
+        Select dropdown = new Select(Objects.requireNonNull(WaitUtil.waitForClickableElement(element, timeout)));
+
+        if (!dropdown.getOptions().isEmpty()) {
+            Random random = new Random();
+            int randomOption;
+            final int maxTries = 5;
+            try {
+                for (int i = 1; i <= maxTries; i++) {
+                    randomOption = random.nextInt(dropdown.getOptions().size());
+                    WebElement randomEl = dropdown.getOptions().get(randomOption);
+
+                    if (randomEl.isEnabled()) {
+                        dropdown.selectByIndex(randomOption);
+                        LOGGER.success("Successfully selected a random dropdown option: " + randomEl.getText());
+                        return;
+                    } else {
+                        LOGGER.warn("Selected option is disabled. Try:" + " " + i + "/" + maxTries);
+                    }
+                }
+            } catch (UnsupportedOperationException e) {
+                LOGGER.warn("Disabled option. Re-selecting....");
+            }
         }
     }
     /*
